a74d685beeaec6c49bfff6abfde99f4208fd0f7c
Phoenix0415 <phoenixx0415@gmail.com>
2023-12-17 17:43:02 +1030

    test2

--- chapter_data_structure/basic_data_types.py
+++ chapter_data_structure/basic_data_types.py
@@ -1,9 +1,6 @@
-# using various basic data types to initialize array
+# using various basic data types to initialize arrays
 numbers: list[int] = [0] * 5
 decimals: list[float] = [0.0] * 5
-# Python 的字符实际上是长度为 1 的字符串
-# Python 
 characters: list[str] = ['0'] * 5
 bools: list[bool] = [False] * 5
-# Python 的列表可以自由存储各种基本数据类型和对象引用
 data = [0, 0.0, 'a', False, ListNode(0)]
