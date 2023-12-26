3d5f9c8cd28f27fd9aa334e1fe5748b0861e5fca
Phoenix0415 <phoenixx0415@gmail.com>
2023-12-26 17:31:41 +1030

    update graph/adjacency matrix

--- python/graph/graph_adjacency_matrix.py
+++ python/graph/graph_adjacency_matrix.py
@@ -6,105 +6,106 @@ from modules import Vertex, print_matrix
 
 
 class GraphAdjMat:
-    """基于邻接矩阵实现的无向图类"""
+    """undirected graph implemented by adjacency matrix"""
 
     def __init__(self, vertices: list[int], edges: list[list[int]]):
-        """构造方法"""
-        # 顶点列表，元素代表“顶点值”，索引代表“顶点索引”
+        """constructor"""
+        # list of vertices. its elements are vertex values, the index of each element is the vertex index
         self.vertices: list[int] = []
-        # 邻接矩阵，行列索引对应“顶点索引”
+        # adjacency matrix, its index is the vertex index
         self.adj_mat: list[list[int]] = []
-        # 添加顶点
+        # add vertices
         for val in vertices:
             self.add_vertex(val)
-        # 添加边
-        # 请注意，edges 元素代表顶点索引，即对应 vertices 元素索引
+        # add edges
+        # note: the edges elements represent vertex indices, that is, the index of each element in vertices
         for e in edges:
             self.add_edge(e[0], e[1])
 
     def size(self) -> int:
-        """获取顶点数量"""
+        """get the number of vertices"""
         return len(self.vertices)
 
     def add_vertex(self, val: int):
-        """添加顶点"""
+        """add vertex"""
         n = self.size()
-        # 向顶点列表中添加新顶点的值
+        # add vertex to vertices list
         self.vertices.append(val)
-        # 在邻接矩阵中添加一行
+        # add a row to adjacency matrix
         new_row = [0] * n
         self.adj_mat.append(new_row)
-        # 在邻接矩阵中添加一列
+        # add a column to adjacency matrix
         for row in self.adj_mat:
             row.append(0)
 
     def remove_vertex(self, index: int):
-        """删除顶点"""
+        """delete vertex"""
+        # if index is out of range, raise IndexError
         if index >= self.size():
             raise IndexError()
-        # 在顶点列表中移除索引 index 的顶点
+        # remove vertex from vertices list
         self.vertices.pop(index)
-        # 在邻接矩阵中删除索引 index 的行
+        # delete row of index from adjacency matrix
         self.adj_mat.pop(index)
-        # 在邻接矩阵中删除索引 index 的列
+        # delete column of index from adjacency matrix
         for row in self.adj_mat:
             row.pop(index)
 
     def add_edge(self, i: int, j: int):
-        """添加边"""
-        # 参数 i, j 对应 vertices 元素索引
-        # 索引越界与相等处理
+        """add edge"""
+        # parameters i, j are vertex indexes
+        # if index is out of range or i == j, raise IndexError. if i == j, it means the edge is a loop
         if i < 0 or j < 0 or i >= self.size() or j >= self.size() or i == j:
             raise IndexError()
-        # 在无向图中，邻接矩阵关于主对角线对称，即满足 (i, j) == (j, i)
+        # in undirected graph, the adjacency matrix is symmetric about the main diagonal
         self.adj_mat[i][j] = 1
         self.adj_mat[j][i] = 1
 
     def remove_edge(self, i: int, j: int):
-        """删除边"""
-        # 参数 i, j 对应 vertices 元素索引
-        # 索引越界与相等处理
+        """delete edge"""
+        # parameters i, j are vertex indexes
+        # if index is out of range or i == j, raise IndexError. if i == j, it means the edge is a loop
         if i < 0 or j < 0 or i >= self.size() or j >= self.size() or i == j:
             raise IndexError()
         self.adj_mat[i][j] = 0
         self.adj_mat[j][i] = 0
 
     def print(self):
-        """打印邻接矩阵"""
-        print("顶点列表 =", self.vertices)
-        print("邻接矩阵 =")
+        """print adjacency matrix"""
+        print("vertices =", self.vertices)
+        print("adjacency matrix =")
         print_matrix(self.adj_mat)
 
 
 """Driver Code"""
 if __name__ == "__main__":
-    # 初始化无向图
-    # 请注意，edges 元素代表顶点索引，即对应 vertices 元素索引
+    # initialize undirected graph
+    # note: the edges elements represent vertex indices, that is, the index of each element in vertices
     vertices = [1, 3, 2, 5, 4]
     edges = [[0, 1], [0, 3], [1, 2], [2, 3], [2, 4], [3, 4]]
     graph = GraphAdjMat(vertices, edges)
-    print("\n初始化后，图为")
+    print("\nafter initialization, graph is")
     graph.print()
 
-    # 添加边
-    # 顶点 1, 2 的索引分别为 0, 2
+    # add edge
+    # the indices of vertices 1, 2 are 0, 2 respectively
     graph.add_edge(0, 2)
-    print("\n添加边 1-2 后，图为")
+    print("\nadd edge 1-2, graph is")
     graph.print()
 
-    # 删除边
-    # 顶点 1, 3 的索引分别为 0, 1
+    # delete edge
+    # the indices of vertices 1, 3 are 0, 1 respectively
     graph.remove_edge(0, 1)
-    print("\n删除边 1-3 后，图为")
+    print("\nafter deleting edge 1-3, graph is")
     graph.print()
 
-    # 添加顶点
+    # add vertex
     graph.add_vertex(6)
-    print("\n添加顶点 6 后，图为")
+    print("\nafter adding vertex 6, graph is")
     graph.print()
 
-    # 删除顶点
-    # 顶点 3 的索引为 1
+    # delete vertex
+    # the index of vertex 3 is 1
     graph.remove_vertex(1)
-    print("\n删除顶点 3 后，图为")
+    print("\nafter deleting vertex 3, graph is")
     graph.print()
