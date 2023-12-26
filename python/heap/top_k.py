2855b17d88321aef970eb5130cefa0513437ce4c
Phoenix0415 <phoenixx0415@gmail.com>
2023-12-26 16:19:33 +1030

    update top-k heap

--- python/heap/top_k.py
+++ python/heap/top_k.py
@@ -8,15 +8,15 @@ import heapq
 
 
 def top_k_heap(nums: list[int], k: int) -> list[int]:
-    """基于堆查找数组中最大的 k 个元素"""
-    # 初始化小顶堆
+    """select top k elements from nums using heap"""
+    # initialize heap
     heap = []
-    # 将数组的前 k 个元素入堆
+    # push first k elements into heap  
     for i in range(k):
-        heapq.heappush(heap, nums[i])
-    # 从第 k+1 个元素开始，保持堆的长度为 k
+        heapq.heappush(heap, nums[i]) 
+    # keep the size of heap to k, and replace the top element if necessary
     for i in range(k, len(nums)):
-        # 若当前元素大于堆顶元素，则将堆顶元素出堆、当前元素入堆
+        # if the current element is larger than the top element of heap, replace it with the current element
         if nums[i] > heap[0]:
             heapq.heappop(heap)
             heapq.heappush(heap, nums[i])
@@ -29,5 +29,5 @@ if __name__ == "__main__":
     k = 3
 
     res = top_k_heap(nums, k)
-    print(f"最大的 {k} 个元素为")
+    print(f"top {k} elements of {nums} are")
     print_heap(res)
